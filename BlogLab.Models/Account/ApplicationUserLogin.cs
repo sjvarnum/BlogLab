1845f6065bc260cee3ba449cd68ae6c2f7d69e63
Shane Varnum <shane.varnum@ftr.com>
2020-12-27 12:58:08 -0500

    Models set up. Account repository created.

--- BlogLab.Models/Account/ApplicationUserLogin.cs
+++ BlogLab.Models/Account/ApplicationUserLogin.cs
@@ -8,13 +8,13 @@ namespace BlogLab.Models.Account
     public class ApplicationUserLogin
     {
         [Required(ErrorMessage = "Username is required")]
-        [MinLength(5, ErrorMessage = "Must be between 5-20 characters in length")]
-        [MaxLength(20, ErrorMessage = "Must be between 5-20 characters in length")]
+        [MinLength(5, ErrorMessage = "Must be 5-20 characters")]
+        [MaxLength(20, ErrorMessage = "Must be 5-20 characters")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-        [MinLength(10, ErrorMessage = "Must be between 10-50 characters in length")]
-        [MaxLength(50, ErrorMessage = "Must be between 10-50 characters in length")]
+        [MinLength(10, ErrorMessage = "Must be 10-50 characters")]
+        [MaxLength(50, ErrorMessage = "Must be 10-50 characters")]
         public string Password { get; set; }
     }
 }
